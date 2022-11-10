77eff1005f040976ec21d2b5dc5c69ef45f82a87
Micheal Parks <michealparks1989@gmail.com>
2022-11-09 20:54:59 -0500

    update deps

---
+++ vite.playground.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
+import wasm from 'vite-plugin-wasm'
+import define from './env'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    minify: true,
+    target: 'esnext',
+  },
+  envPrefix: ['SWORD'],
+  plugins: [
+    mkcert(),
+    wasm(),
+  ],
+  publicDir: 'assets',
+  server: {
+    fs: {
+      allow: ['.'],
+      strict: true,
+    },
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+    port: 5173,
+    strictPort: true,
+  },
+  define,
+})
